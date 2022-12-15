9dd1eb14d7f58d7643e2797e3131e9351833204e
Kevin Dutzy <kddutz15@gmail.com>
2022-12-15 14:33:14 -0500

    commit tree 4a1126b1fd0e91014337a8c411948becf9d3768b

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative "user.rb"
+require 'pry'
+class Student < User
 
+attr_accessor :knowledge
+
+    def initialize(knowledge = [])
+        @knowledge = knowledge
+    end
+
+    def learn(string)
+        self.knowledge << string
+    end
 end
+kevin = Student.new
+kevin.learn("synth stuff")
+
